492485eee71b99261d0af443ee6c92f4b8e8e744
Sotatsu57 <tassei.moriyama@gmail.com>
2024-06-15 18:49:14 +0900

    ファイル構造を整理した

---
+++ internal/handler/room.go
@@ -0,0 +1,41 @@
+package handler
+
+import (
+	"h24s_19/internal/repository"
+	"net/http"
+
+	"github.com/google/uuid"
+	"github.com/labstack/echo/v4"
+)
+
+type Room struct {
+	RoomId   uuid.UUID `db:"room_id"`
+	RoomName string    `db:"room_name"`
+	IsPublic bool      `db:"is_public"`
+}
+
+type RoomRequest struct {
+	RoomName string `json:"room_name"`
+	IsPublic bool   `json:"is_public"`
+	Password string `json:"password"`
+}
+
+func (h *Handler) GetRooms(c echo.Context) error {
+	rooms, err := h.repo.GetRooms(c.Request().Context())
+	if err != nil {
+		return echo.NewHTTPError(http.StatusInternalServerError).SetInternal(err)
+	}
+	return c.JSON(http.StatusOK, rooms)
+}
+
+func (h *Handler) CreateRoom(c echo.Context) error {
+	var params RoomRequest
+	if err := c.Bind(&params); err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest).SetInternal(err)
+	}
+	room, err := h.repo.CreateRoom(c.Request().Context(), repository.RoomRequest{params.RoomName, params.IsPublic, params.Password})
+	if err != nil {
+		return echo.NewHTTPError(http.StatusInternalServerError).SetInternal(err)
+	}
+	return c.JSON(http.StatusCreated, room)
+}
