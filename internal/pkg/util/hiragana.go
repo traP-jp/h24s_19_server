3be4c47eef1a1e7e85f3fc42f00eb90df4888445
Sotatsu57 <tassei.moriyama@gmail.com>
2024-06-16 11:36:46 +0900

    util 修正 table追加

---
+++ internal/pkg/util/hiragana.go
@@ -0,0 +1,11 @@
+package util
+
+func StringToRuneSlice(s string) []rune {
+	return []rune(s)
+}
+
+func RuneSliceToString(runes []rune) string {
+	return string(runes)
+}
+
+
---
+++ sql/init.sql
@@ -0,0 +1,16 @@
+DROP TABLE IF EXISTS `rooms`;
+CREATE TABLE `rooms` (
+  `room_id` VARCHAR(36) NOT NULL,
+  `room_name` varchar(30) NOT NULL,
+  `is_public` BOOLEAN NOT NULL,
+  PRIMARY KEY (`room_id`)
+);
+
+DROP TABLE IF EXISTS `rune_counts`;
+CREATE TABLE `rune_counts` (
+  `room_id` VARCHAR(36) NOT NULL,
+  `rune` varchar(10) NOT NULL,
+  `count` INTEGER NOT NULL,
+  PRIMARY KEY (`room_id`, `rune`),
+  FOREIGN KEY (`room_id`) REFERENCES `rooms`(`room_id`)
+);
